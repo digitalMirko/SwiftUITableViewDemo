960c3ef174bb86811c101e431e59eb36fced0f3b
Mirko Cukich <mirkocukich@gmail.com>
2019-08-05 23:43:35 -0500

    Initial Commit

---
+++ UITable View Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITable View Demo
+//
+//  Created by Mirko Cukich on 8/5/19.
+//  Copyright © 2019 Digital Mirko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
