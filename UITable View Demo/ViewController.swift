116074d140b0e5b7f35b478e33ffa0689ecbcdfa
Mirko Cukich <mirkocukich@gmail.com>
2019-08-06 23:30:17 -0500

    Resources and Constraints placed on storyboard

--- UITable View Demo/ViewController.swift
+++ UITable View Demo/ViewController.swift
@@ -5,17 +5,18 @@
 //  Created by Mirko Cukich on 8/5/19.
 //  Copyright © 2019 Digital Mirko. All rights reserved.
 //
-//  Swift UITable View Demo
+//  Swift UITable View Demo - Demo 5 of 30
 
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    
 
 }
 
