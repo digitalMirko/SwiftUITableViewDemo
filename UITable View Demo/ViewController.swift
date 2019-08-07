f647931e80e964e57dc92119051f210b35768bc6
Mirko Cukich <mirkocukich@gmail.com>
2019-08-06 23:45:11 -0500

    Program Complete

--- UITable View Demo/ViewController.swift
+++ UITable View Demo/ViewController.swift
@@ -8,15 +8,30 @@
 //  Swift UITable View Demo - Demo 5 of 30
 
 import UIKit
+// Two delegates required: Table View > Delegate and Data Source
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-class ViewController: UIViewController {
-    
+    // Table View Outlet
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //needed to make it work
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
 
+    // these two are req. for table view to be implemented
+    // total number of rows in table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 60
+    }
     
-
+    // row contents > ex. Row #20 / where you populate,
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        // calls out which row in use displayed
+        cell?.textLabel?.text = "Row #\(indexPath.row)"
+        return cell!
+    }
 }
-
